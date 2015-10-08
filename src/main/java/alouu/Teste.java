51c5a2c973c03043d2b82cf1afa49346671a2ac0
paulo <paulo.szpikula10@hotmail.com>
2015-10-08 20:37:00 -0300

    Segundo comit

--- src/main/java/alouu/Teste.java
+++ src/main/java/alouu/Teste.java
@@ -6,3 +6,6 @@ public class Teste {
 		System.out.println("Alô!!");
 	}
 }
+
+// git não grava repositórios vazios
+// então dentro do main tem que ter uma sequência de pastas, pra isso basta criar um novo projeto e ver a estrutura
